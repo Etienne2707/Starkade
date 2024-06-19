f6472a87168b7a1903d3bca7251e30516c4975c8
Damien Delhalt <d.delhalt@gmail.com>
2024-06-19 18:11:06 +0000

    Small improvements to the project init script

--- init_game.sh
+++ init_game.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+# Create project
 make CONTAINER=podman GAME_INIT=$1 game_init
+
+# Cleanup project
+rm -rf starkade-cairo/$1/.git
+rm -rf starkade-cairo/$1/.github
+rm -rf starkade-cairo/$1/.gitignore
+rm -rf starkade-cairo/$1/.vscode
+
+# New additions
 cp ./dojo_make starkade-cairo/$1/Makefile
 sed -i "1s/^/GAME = $1\n/" starkade-cairo/$1/Makefile
+echo "/__container_cache__" > starkade-cairo/$1/.gitignore
